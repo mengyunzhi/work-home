d2585235b32c48937a53069853ce4de842cf77c2
panjie <3792535@qq.com>
2020-03-20 19:51:23 +0800

    init

---
+++ api/src/main/java/club/yunzhi/workhome/WorkHomeApplication.java
@@ -0,0 +1,14 @@
+package club.yunzhi.workhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yunzhi
+ */
+@SpringBootApplication
+public class WorkHomeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WorkHomeApplication.class, args);
+	}
+}
---
+++ api/src/test/java/club/yunzhi/workhome/WorkHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package club.yunzhi.workhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
