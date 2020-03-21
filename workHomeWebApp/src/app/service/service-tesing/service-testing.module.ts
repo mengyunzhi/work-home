b5eddd78ea725f7ccad1b7097bd3dbc02a1f9b8e
panjie <3792535@qq.com>
2020-03-21 08:42:22 +0800

    add UserService stub

--- workHomeWebApp/src/app/service/service-tesing/service-testing.module.ts
+++ workHomeWebApp/src/app/service/service-tesing/service-testing.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {CommonService} from '../common.service';
 import {CommonStubService} from './common-stub.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-
+import {UserService} from '../user.service';
+import {UserStubService} from './user-stub.service';
 
 
 @NgModule({
@@ -16,7 +17,9 @@ import {HttpClientTestingModule} from '@angular/common/http/testing';
     HttpClientTestingModule
   ],
   providers: [
-    {provide: CommonService, useClass: CommonStubService}
+    {provide: CommonService, useClass: CommonStubService},
+    {provide: UserService, useClass: UserStubService},
   ]
 })
-export class ServiceTestingModule { }
+export class ServiceTestingModule {
+}
