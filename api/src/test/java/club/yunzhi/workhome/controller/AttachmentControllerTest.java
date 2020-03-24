97b8dfb16e4ffd9c984f7fd8bd34a5a38ad86f59
liyiheng <2287217556@qq.com>
2020-03-24 18:39:34 +0800

    学生上传文件保存到自己目录下后台接口

---
+++ api/src/test/java/club/yunzhi/workhome/controller/AttachmentControllerTest.java
@@ -0,0 +1,52 @@
+package club.yunzhi.workhome.controller;
+
+import club.yunzhi.workhome.service.AttachmentService;
+import club.yunzhi.workhome.service.WorkService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.util.ResourceUtils;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class AttachmentControllerTest extends ControllerTest {
+    private static final Logger logger = LoggerFactory.getLogger(AttachmentControllerTest.class);
+
+    private String url = "/attachment";
+
+    @Autowired
+    private ResourceLoader loader;
+
+    @MockBean
+    AttachmentService attachmentService;
+
+    @Test
+    void uploadWork() throws Exception {
+        logger.debug("定义常量");
+        final String NAME = "attachment";
+        final String FILE_NAME = "example.jpeg";
+
+        logger.debug("获取资源");
+        Resource resource = loader.getResource(ResourceUtils.CLASSPATH_URL_PREFIX + FILE_NAME);
+
+        logger.debug("创建模拟文件");
+        MockMultipartFile multipartFile = new MockMultipartFile(NAME, FILE_NAME, "image/jpeg", resource.getInputStream());
+
+        logger.debug("断言请求成功");
+        this.mockMvc.perform(multipart("/attachment/uploadWork").file(multipartFile))
+                .andExpect(status().isOk());
+
+        logger.debug("断言方法调用成功");
+        Mockito.verify(attachmentService).uploadWork(multipartFile);
+    }
+}
