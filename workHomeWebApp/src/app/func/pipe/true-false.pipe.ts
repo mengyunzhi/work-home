f94da980d0c15b4381fb5e9408da0235ce48de17
LYX6666666 <1098659745@qq.com>
2020-04-15 21:17:24 +0800

    增加过滤器，显示是否

--- workHomeWebApp/src/app/func/pipe/true-false.pipe.ts
+++ workHomeWebApp/src/app/func/pipe/true-false.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 /*
- * tf管道
+ * tf管道 刘宇轩
  * 使用 | tf
  * 作用 把true false 变为是否
 */
-@Pipe({name: 'tf'})
+@Pipe({name: 'trueFalse'})
 export class TrueFalsePipe implements PipeTransform {
   transform(value: boolean): string {
+    if (value === null || value === undefined) {
+      return '-';
+    }
     return value ? '是' : '否';
   }
 }
