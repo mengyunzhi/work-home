055795837e9dc4cec39597a4bf02fbda36a938b6
zhaokaiqiangZzz <2391945695@qq.com>
2020-05-27 22:17:33 +0800

    238

--- api/src/main/java/club/yunzhi/workhome/repository/WorkRepository.java
+++ api/src/main/java/club/yunzhi/workhome/repository/WorkRepository.java
@@ -47,10 +47,10 @@ public interface WorkRepository extends PagingAndSortingRepository<Work, Long>, 
                 .and(WorkSpecs.belongToItem(item));
         if (reviewed != null) {
             if (!reviewed) {
-                specification = specification.and(WorkSpecs.notIsStatus((short) 2));
+                specification = specification.and(WorkSpecs.notIsStatus((short) 2))
+                .and(WorkSpecs.notIsUserId(lastReviewedUserId));
             } else if (reviewed) {
-                specification = specification.and(WorkSpecs.isStatus((short) 2))
-                        .and(WorkSpecs.notIsUserId(lastReviewedUserId));
+                specification = specification.and(WorkSpecs.isStatus((short) 2));
             }
         }
         return this.findAll(specification, pageable);
