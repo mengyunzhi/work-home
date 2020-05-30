0ed2aee05368893119924478e3ca4a6ddbb12115
LYX6666666 <1098659745@qq.com>
2020-05-30 21:35:43 +0800

    使用flyway在历史数据上更新数据表

--- api/src/main/resources/db/migration/V1.1.0__init.sql
+++ api/src/main/resources/db/migration/V1.1.0__init.sql
@@ -1,3 +1,9 @@
+ALTER TABLE `work`
+CHANGE COLUMN `reviewed` `status` smallint(10) NULL DEFAULT NULL AFTER `create_time`;
+ALTER TABLE `user`
+ADD COLUMN `admin` bit(1) NOT NULL DEFAULT 0 AFTER `role`;
+ALTER TABLE `work`
+ADD COLUMN `last_reviewed_user_id` bigint(20) NOT NULL DEFAULT 0 AFTER `student_id`;
 UPDATE `work`
 SET STATUS=2
 WHERE STATUS=1;
