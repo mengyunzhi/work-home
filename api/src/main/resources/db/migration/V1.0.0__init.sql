d4ad185e8e2fd981c5d6480831064d7bfc46630c
LYX6666666 <1098659745@qq.com>
2020-05-30 22:38:09 +0800

    线上版本导出的结构作为1.0.0

--- api/src/main/resources/db/migration/V1.0.0__init.sql
+++ api/src/main/resources/db/migration/V1.0.0__init.sql
@@ -1,7 +1,7 @@
 /*
- Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
- Source Server         : localhost
+ Source Server         : localhost3306
  Source Server Type    : MySQL
  Source Server Version : 50729
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 50729
  File Encoding         : 65001
 
- Date: 28/05/2020 20:14:28
+ Date: 30/05/2020 22:34:29
 */
 
 SET NAMES utf8mb4;
@@ -21,146 +21,125 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for attachment
 -- ----------------------------
 DROP TABLE IF EXISTS `attachment`;
-CREATE TABLE `attachment`  (
+CREATE TABLE `attachment` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `mime` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `create_time` bigint(20) NULL DEFAULT NULL,
-  `ext` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `md5` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `origin_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `save_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `save_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `sha1` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `size` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `update_time` bigint(20) NULL DEFAULT NULL,
-  `create_user_id` bigint(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `FK3bw3o0qnhedub4rh5s0vnu889`(`create_user_id`) USING BTREE,
-  CONSTRAINT `FK3bw3o0qnhedub4rh5s0vnu889` FOREIGN KEY (`create_user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 93 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for flyway_schema_history
--- ----------------------------
-DROP TABLE IF EXISTS `flyway_schema_history`;
-CREATE TABLE `flyway_schema_history`  (
-  `installed_rank` int(11) NOT NULL,
-  `version` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `description` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `type` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `script` varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `checksum` int(11) NULL DEFAULT NULL,
-  `installed_by` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `installed_on` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-  `execution_time` int(11) NOT NULL,
-  `success` tinyint(1) NOT NULL,
-  PRIMARY KEY (`installed_rank`) USING BTREE,
-  INDEX `flyway_schema_history_s_idx`(`success`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+  `mime` varchar(255) DEFAULT NULL,
+  `create_time` bigint(20) DEFAULT NULL,
+  `ext` varchar(255) DEFAULT NULL,
+  `md5` varchar(255) DEFAULT NULL,
+  `origin_name` varchar(255) DEFAULT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `save_name` varchar(255) DEFAULT NULL,
+  `save_path` varchar(255) DEFAULT NULL,
+  `sha1` varchar(255) DEFAULT NULL,
+  `size` varchar(255) DEFAULT NULL,
+  `update_time` bigint(20) DEFAULT NULL,
+  `create_user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK3bw3o0qnhedub4rh5s0vnu889` (`create_user_id`),
+  CONSTRAINT `FK3bw3o0qnhedub4rh5s0vnu889` FOREIGN KEY (`create_user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16834 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for item
 -- ----------------------------
 DROP TABLE IF EXISTS `item`;
-CREATE TABLE `item`  (
+CREATE TABLE `item` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `begin_time` datetime(0) NULL DEFAULT NULL,
-  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `dir` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `end_time` datetime(0) NULL DEFAULT NULL,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  `begin_time` datetime DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `dir` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for item_attachments
 -- ----------------------------
 DROP TABLE IF EXISTS `item_attachments`;
-CREATE TABLE `item_attachments`  (
+CREATE TABLE `item_attachments` (
   `item_id` bigint(20) NOT NULL,
   `attachments_id` bigint(20) NOT NULL,
-  INDEX `FKcbaqqmgmf82wyn654xhrpy6pu`(`attachments_id`) USING BTREE,
-  INDEX `FK6ne7fgmigv15e8w1g3ojvdhrh`(`item_id`) USING BTREE,
-  CONSTRAINT `FK6ne7fgmigv15e8w1g3ojvdhrh` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `FKcbaqqmgmf82wyn654xhrpy6pu` FOREIGN KEY (`attachments_id`) REFERENCES `attachment` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  KEY `FKcbaqqmgmf82wyn654xhrpy6pu` (`attachments_id`),
+  KEY `FK6ne7fgmigv15e8w1g3ojvdhrh` (`item_id`),
+  CONSTRAINT `FK6ne7fgmigv15e8w1g3ojvdhrh` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`),
+  CONSTRAINT `FKcbaqqmgmf82wyn654xhrpy6pu` FOREIGN KEY (`attachments_id`) REFERENCES `attachment` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for student
 -- ----------------------------
 DROP TABLE IF EXISTS `student`;
-CREATE TABLE `student`  (
+CREATE TABLE `student` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `no` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `no` varchar(255) NOT NULL,
   `user_id` bigint(20) NOT NULL,
-  `average_score` int(20) NULL DEFAULT NULL,
-  `total_score` int(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `FKk5m148xqefonqw7bgnpm0snwj`(`user_id`) USING BTREE,
-  CONSTRAINT `FKk5m148xqefonqw7bgnpm0snwj` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  `average_score` int(11) DEFAULT NULL,
+  `total_score` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKk5m148xqefonqw7bgnpm0snwj` (`user_id`),
+  CONSTRAINT `FKk5m148xqefonqw7bgnpm0snwj` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for teacher
 -- ----------------------------
 DROP TABLE IF EXISTS `teacher`;
-CREATE TABLE `teacher`  (
+CREATE TABLE `teacher` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `user_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `FKpb6g6pahj1mr2ijg92r7m1xlh`(`user_id`) USING BTREE,
-  CONSTRAINT `FKpb6g6pahj1mr2ijg92r7m1xlh` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`),
+  KEY `FKpb6g6pahj1mr2ijg92r7m1xlh` (`user_id`),
+  CONSTRAINT `FKpb6g6pahj1mr2ijg92r7m1xlh` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
+CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `role` int(11) NOT NULL,
-  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `admin` bit(1) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `UK_sb8bbouer5wak8vyiiy4pf2bx`(`username`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=124 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for work
 -- ----------------------------
 DROP TABLE IF EXISTS `work`;
-CREATE TABLE `work`  (
+CREATE TABLE `work` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
-  `create_time` datetime(0) NULL DEFAULT NULL,
-  `status` smallint(10) NULL DEFAULT NULL,
-  `score` int(11) NULL DEFAULT NULL,
-  `update_time` datetime(0) NULL DEFAULT NULL,
+  `content` text,
+  `create_time` datetime DEFAULT NULL,
+  `reviewed` bit(1) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
   `item_id` bigint(20) NOT NULL,
   `student_id` bigint(20) NOT NULL,
-  `last_reviewed_user_id` bigint(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `FK3c5vyiblbbyt0dda0yjbs97k7`(`item_id`) USING BTREE,
-  INDEX `FK3x7nfcg2v6itg7juqm487qi55`(`student_id`) USING BTREE,
-  CONSTRAINT `FK3c5vyiblbbyt0dda0yjbs97k7` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `FK3x7nfcg2v6itg7juqm487qi55` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`),
+  KEY `FK3c5vyiblbbyt0dda0yjbs97k7` (`item_id`),
+  KEY `FK3x7nfcg2v6itg7juqm487qi55` (`student_id`),
+  CONSTRAINT `FK3c5vyiblbbyt0dda0yjbs97k7` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`),
+  CONSTRAINT `FK3x7nfcg2v6itg7juqm487qi55` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=320 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for work_attachments
 -- ----------------------------
 DROP TABLE IF EXISTS `work_attachments`;
-CREATE TABLE `work_attachments`  (
+CREATE TABLE `work_attachments` (
   `work_id` bigint(20) NOT NULL,
   `attachments_id` bigint(20) NOT NULL,
-  INDEX `FKgsxomix0uyqntrw79xybxdfjg`(`attachments_id`) USING BTREE,
-  INDEX `FKjdxbhknbb5g1tl5hsj9yvgab5`(`work_id`) USING BTREE,
-  CONSTRAINT `FKgsxomix0uyqntrw79xybxdfjg` FOREIGN KEY (`attachments_id`) REFERENCES `attachment` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `FKjdxbhknbb5g1tl5hsj9yvgab5` FOREIGN KEY (`work_id`) REFERENCES `work` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  KEY `FKgsxomix0uyqntrw79xybxdfjg` (`attachments_id`),
+  KEY `FKjdxbhknbb5g1tl5hsj9yvgab5` (`work_id`),
+  CONSTRAINT `FKgsxomix0uyqntrw79xybxdfjg` FOREIGN KEY (`attachments_id`) REFERENCES `attachment` (`id`),
+  CONSTRAINT `FKjdxbhknbb5g1tl5hsj9yvgab5` FOREIGN KEY (`work_id`) REFERENCES `work` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 SET FOREIGN_KEY_CHECKS = 1;
